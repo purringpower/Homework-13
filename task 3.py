92a1420af7667005bfa29bcf432677fd9ee4a9f0
Maryna <doctormrrr@gmail.com>
2021-04-27 02:07:35 +0300

    started task 3

---
+++ task 3.py
@@ -0,0 +1,10 @@
+class Group:
+    class Student:
+        def __init__(self, name, age, grades):
+            self.name = name
+            self.age = age
+            self.grades = grades
+
+        def get_name(self):
+            print(self.name)
+
